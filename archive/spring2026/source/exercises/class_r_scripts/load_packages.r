4b1fb55274ea1af3922207b665d04795ee86ec01
Patrick Kelley <kelley@post.harvard.edu>
2026-08-27 08:52:27 -0600

    Created archived copy of Spring 2026 course.

---
+++ archive/spring2026/source/exercises/class_r_scripts/load_packages.r
@@ -0,0 +1,33 @@
+# The following code loads the libraries into your R session.
+
+# Package names
+packages <- c(
+  "ggplot2",
+  "seewave",
+  "tuneR",
+  "ggeffects",
+  "GGally",
+  "MuMIn",
+  "insight",
+  "rio",
+  "Matrix",
+  "dplyr",
+  "tidyr",
+  "reshape2",
+  "lubridate",
+  "psych",
+  "stringr",
+  "tidyverse",
+  "fields",
+  "mgcv",
+  "MASS",
+  "readr")
+
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+# Packages loading
+invisible(lapply(packages, library, character.only = TRUE))
